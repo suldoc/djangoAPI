d5730ff4e63fcc9280e69f632ee7db3618e1d37d
Yvonne Lee <sparrow_hh@naver.com>
2019-08-27 21:38:13 +0900

    add Database

--- suldocs/models.py
+++ suldocs/models.py
@@ -5,9 +5,11 @@ class Suldoc(models.Model):
     user_id = models.CharField(max_length=30) # 유저 아이디
     name = models.CharField(max_length=100) # 술 이름
     comment = models.CharField(max_length=200) # 코멘트
-    stars_taste = models.IntegerField() # 맛 평가
-    stars_costvalue = models.IntegerField() # 가성비 평가
+    stars_taste = models.FloatField() # 맛 평가
+    stars_costvalue = models.FloatField() # 가성비 평가
     img_path = models.CharField(max_length=1000) # 이미지 경로
+    create_at = models.DateTimeField(auto_now_add=True) # 작성 시간
+    update_at = models.DateTimeField(auto_now=True)  # 수정시간
 
     def __str__(self):
-        return self.title
+        return self.name
